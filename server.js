4f9d9a7d905d9da77028d820d7bb871ef4ba5b43
Krzysiek Szymczak <rakaz666@gmail.com>
2016-03-02 21:42:39 +0100

    dużo logów

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 //setup
 var azure = require('azure');
 var port = process.env.PORT | 1337;
+console.log("PORT: ",port);
 var mongoDbuser = "";
 var mongoDbpass = "";
 azure.RoleEnvironment.getConfigurationSettings(function(error, settings) {
   if (!error) {
       mongoDbpass = settings['dbpass'];
       mongoDbuser = settings['dbuser'];
+      console.log(mongoDbAccess);
+      console.log(mongoDbpass);
   }
 });
 
 var mongoDbAccess = "mongodb://"+mongoDbuser+":"+mongoDbpass+"@ds064188.mlab.com:64188/loteriaparagonowaapi"
+console.log(mongoDbAccess);
 var mongoose = require('mongoose');
-mongoose.connect(mongoDbAccess);
+//mongoose.connect(mongoDbAccess);
 var lottery = require('./lotteryModel');
+console.log(lottery);
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -22,6 +27,7 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var lotteryController = require('./lotteryController');
+console.log(lotteryController);
 
 app.get('/', function(req,res){
    res.send("It works"); 
