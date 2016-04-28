0fad862e206cdf31eef451288afbe3244b422e2d
Krzysiek Szymczak <rakaz666@gmail.com>
2016-04-28 21:13:05 +0200

    semi-final version

--- app.js
+++ app.js
@@ -2,24 +2,40 @@
 var port = process.env['PORT'] || 1337;
 console.log("PORT: ",port);
 
-var mongoDbuser = process.env['dbuser'];
-var mongoDbpass = process.env['dbpass'];
+var mongoUrl = "91.185.190.13";
+var monogDbName = "mo12699_lotApi";
 
-var mongoDbAccess = "mongodb://"+mongoDbuser+":"+mongoDbpass+"@ds064188.mlab.com:64188/loteriaparagonowaapi"
+var mongoDbuser = null;
+var mongoDbpass = null;
+
+if(!process.env['dbuser']){
+    var env = require('./env');
+    mongoDbuser = env.dbuser;
+    mongoDbpass = env.dbpass;
+    env = undefined;
+}
+else{
+    mongoDbuser = process.env['dbuser'];
+    mongoDbpass = process.env['dbpass'];    
+}
+
+var mongoDbAccess = "mongodb://"+mongoDbuser+":"+mongoDbpass+"@"+mongoUrl+"/"+monogDbName;
 var mongoose = require('mongoose');
 mongoose.connect(mongoDbAccess);
 var lottery = require('./lotteryModel');
 var express = require('express');
+var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser');
 
+app.use(cors());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 var lotteryController = require('./lotteryController');
 
 app.get('/', function(req,res){
-   res.send("It works"); 
+   res.send('It works'); 
 });
 
 app.get('/lottery/:nr', lotteryController.findByNr);
--- lotteryModel.js
+++ lotteryModel.js
@@ -6,7 +6,7 @@ var Schema = mongoose.Schema;
 var lotterySchema = new Schema({
   nrkasy:String,
   nip:String
-});
+}, {collection:'loteriaParagonowa'});
 
 
 var lottery = mongoose.model('lottery', lotterySchema );
